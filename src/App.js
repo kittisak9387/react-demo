eddbfc6532e29998c91735fb28254ccc34c3f837
kittisak9387 <kittisakb.9387@gmail.com>
2022-06-19 23:57:05 +0700

    react-demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const var1 = 0;
+  const var2 = "test";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{var1}</h1>
+      <h2>{var2}</h2>
     </div>
   );
 }
